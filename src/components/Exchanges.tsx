1ae07a3fa43f0afe5b9c525ffe188c68a42971c0
Elijah <elijahadewunmi2005@gmail.com>
2024-11-19 23:41:56 +0100

    final commit

--- src/components/Exchanges.tsx
+++ src/components/Exchanges.tsx
@@ -18,20 +18,21 @@ const { Text } = Typography;
 const { Panel } = Collapse;
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetCryptosQuery(10);
-  const exchangesList = data?.data?.coin;
+  const { data, isFetching } = useGetCryptosQuery(20);
+  const exchangesList = data?.data?.coins;
+
   if (isFetching) return <Loader />;
 
   return (
     <>
-      <Row>
+      <Row style={{ marginBottom: "20px" }}>
         <Col span={6}>Exchanges</Col>
         <Col span={6}>24h Trade Volume</Col>
         <Col span={6}>Markets</Col>
         <Col span={6}>Change</Col>
       </Row>
       <Row>
-        {exchangesList.map((exchange: Currency) => (
+        {exchangesList?.map((exchange: Currency) => (
           <Col span={24}>
             <Collapse>
               <Panel
@@ -58,11 +59,11 @@ const Exchanges = () => {
                 }
               >
                 <p>
-                  {" "}
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Debitis atque quam quisquam voluptatibus nobis magnam. Beatae
-                  dignissimos, asperiores atque quaerat unde architecto odit
-                  quisquam, at tenetur illo recusandae ullam rerum!{" "}
+                  Necessitatibus, velit temporibus cumque voluptatibus nesciunt
+                  maiores minima? Ducimus repellat fuga sunt pariatur distinctio
+                  iure in! Soluta culpa, similique, officia dolores laudantium
+                  quidem odit at nostrum ipsam error mollitia quis ad quasi.
                 </p>
               </Panel>
             </Collapse>
